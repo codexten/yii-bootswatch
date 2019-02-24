1a53494b4dc5b00f2bf03f7e0802fb7c7a547f93
Jomon Johnson <jomon.entero@gmail.com>
2019-02-24 19:43:21 +0530

    init

---
+++ config/bootswatch.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jomon
+ * Date: 24/2/19
+ * Time: 7:27 PM
+ */
+
+return [
+    'components' => [
+        'themeManager' => [
+            'defaultTheme' => 'bootswatchFlatly',
+            'themes' => [
+                'bootswatchFlatly' => [
+                    'class' => \codexten\yii\web\Theme::class,
+                ],
+            ],
+        ],
+    ],
+];
